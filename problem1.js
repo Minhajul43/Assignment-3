656fc0f5954fa9bbef9a7a024148b31cd108a158
Minhajul43 <minhajul255685islam@gmail.com>
2026-02-06 22:50:52 +0600

    problem1 done

--- problem1.js
+++ problem1.js
@@ -1,6 +1,20 @@
 /*function signature/sample */
 function newPrice(currentPrice , discount ) {
-  // You have to write your code here
-  
+  // zzYou have to write your code here
+  if (typeof currentPrice !== "number"
+      || typeof discount !== "number"
+      || discount > 100
+      || discount < 0) {
+    return 'Invalid';
+  }
+
+  else {
+    let discountPrice = currentPrice * (discount / 100);
+    let newPrice = currentPrice - discountPrice;
+    return newPrice.toFixed(3);
+  }
 }
+
+const price = newPrice(2000, 20);
+console.log(price);
 
